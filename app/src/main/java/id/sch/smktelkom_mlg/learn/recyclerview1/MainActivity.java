4b5649c790a18fe9f3728649a8aa96c1b00907b3
bima <bimacipir@gmail.com>
2017-02-09 22:58:43 +0700

    RecyclerView List Round Image

--- app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview1/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview1/MainActivity.java
@@ -32,7 +32,6 @@ public class MainActivity extends AppCompatActivity {
 
         fillData();
     }
-
     private void fillData() {
         Resources resources = getResources();
         String[] arJudul = resources.getStringArray(R.array.places);
